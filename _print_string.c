20376e982f39f8f3b6a015639c4b2f6db193ff8f
= <=>
2023-04-22 21:05:51 +0000

    Update _put_string to fix an eage case (NULL)

--- _print_string.c
+++ _print_string.c
@@ -9,11 +9,23 @@
 */
 void _print_string(char *buffer, int *x, char *value_c)
 {
-	int y = 0;
+	int y, i;
+	char *str = "(null)";
 
-	for (y = 0; value_c[y] != '\0'; y++)
+	if (value_c == NULL)
 	{
-		buffer[*x] = value_c[y];
-		(*x)++;
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			buffer[*x] = str[i];
+			(*x)++;
+		}
+	}
+	else
+	{
+		for (y = 0; value_c[y] != '\0'; y++)
+		{
+			buffer[*x] = value_c[y];
+			(*x)++;
+		}
 	}
 }
