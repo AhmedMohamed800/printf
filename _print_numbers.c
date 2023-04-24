adb18594c8f5150fc446ab0f3bfb9ae05924001a
KogingahO <kogingah@gmail.com>
2023-04-24 15:02:55 -0700

    print numbers

---
+++ _print_numbers.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+* _print_numbers - Prints integer
+* @args: value of arguments
+*
+* Return: numbers of characters printed
+*/
+int _print_numbers(va_list args)
+{
+	int num, digits, temp, count;
+	unsigned int i;
+
+	count = 0;
+
+	i = va_arg(args, int);
+
+	if (i < 0)
+	{
+		num = (i * -1);
+		_putchar(45);
+		count += 1;
+	}
+	else
+		num = i;
+
+	digits = num;
+	temp = 1;
+	while (digits > 9)
+	{
+		digits /= 10;
+		temp *= 10;
+	}
+
+	while (temp >= 1)
+	{
+		count += 1;
+		_putchar(((num / temp) % 10) + '0');
+		temp /= 10;
+	}
+	return (count);
+}
+
+/**
+ * _print_integer - prints integers
+ * @args: value of arguments
+ *
+ * Return: number of characters to be printed
+ */
+
+int _print_integer(va_list args)
+{
+	return (_print_numbers(args));
+}
