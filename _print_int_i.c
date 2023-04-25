0e7cee5fa700d76f70c8a2d84ecd89a3cac8ad52
= <=>
2023-04-25 09:20:50 +0000

    Change _print_unsign to _print_ini_i

---
+++ _print_int_i.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* _print_int_i - prints integer
+* @args: format argument
+* Return: number's length
+*/
+int _print_int_i(va_list args)
+{
+	return (_print_integer(args));
+}
--- _print_integer.c
+++ _print_integer.c
@@ -8,33 +8,30 @@
 */
 int _print_integer(va_list args)
 {
-	int num = va_arg(args, int), n = num;
+	int num = va_arg(args, int);
 	int digits, count = 0;
 
-	if (n == 0)
+	if (num == 0)
 	{
 		_putchar('0');
 		return (1);
 	}
-	if (n < 0)
+	if (num < 0)
 	{
-		num = -n;
+		num = num * -1;
 		_putchar('-');
 		count++;
-	}
-	else
-	{
-		num = n;
 	}
 
 	digits = 1;
 	while ((num / digits) >= 10)
+	{
 		digits *= 10;
-
+	}
 	while (digits != 0)
 	{
 		count += 1;
-		_putchar((num / digits) % 10 + '0');
+		_putchar(((num / digits) % 10) + '0');
 		digits /= 10;
 	}
 
